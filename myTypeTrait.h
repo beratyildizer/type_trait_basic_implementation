c631a93bec9a232568eb37ab7b1be67aa48a59d8
Berat Yıldızer <44676948+beratyildizer@users.noreply.github.com>
2023-06-08 21:04:13 +0300

    Add files via upload

--- myTypeTrait.h
+++ myTypeTrait.h
@@ -25,10 +25,24 @@ void doSomething(T x, FalseType y)
 }
 
 template<typename T>
+class isArray : public FalseType {};
+
+template<typename T>
+class isArray<T[]> : public TrueType {};
+
+template<typename T>
+auto constexpr isArray_v = isArray<T>::value;
+
+
+template<typename T>
 class IsVoid : public FalseType {};
 
 template<>
 class IsVoid<void> : public TrueType {};
+
+template<typename T>
+auto constexpr IsVoid_v = IsVoid<T>::value;
+
 
 template<typename T>
 class IsNullPointer : public FalseType {};
@@ -37,10 +51,18 @@ template<>
 class IsNullPointer<std::nullptr_t> : public TrueType {};
 
 template<typename T>
+auto constexpr IsNullPointer_v = IsNullPointer<T>::value;
+
+
+template<typename T>
 class IsIntegral : public FalseType {};
 
 template<>
 class IsIntegral<int> : public TrueType {};
+
+template<typename T>
+auto constexpr IsIntegral_v = IsIntegral<T>::value;
+
 
 template<typename T>
 class IsFloatingPoint : public FalseType {};
@@ -49,10 +71,18 @@ template<>
 class IsFloatingPoint<int> : public TrueType {};
 
 template<typename T>
+auto constexpr IsFloatingPoint_v = IsFloatingPoint<T>::value;
+
+
+template<typename T>
 class IsPointer : public FalseType {};
 
 template<typename T>
 class IsPointer<T*> : public TrueType {};
+
+template<typename T>
+auto constexpr IsPointer_v = IsPointer<T>::value;
+
 
 template<typename T>
 class IsConst : public FalseType {};
@@ -60,11 +90,20 @@ class IsConst : public FalseType {};
 template<typename T>
 class IsConst<const T> : public TrueType {};
 
+template<typename T>
+auto constexpr IsConst_v = IsConst<T>::value;
+
+
 template<typename T, typename U>
 class IsSame : public FalseType {};
 
 template<typename T>
 class IsSame<T,T> : public TrueType {};
+
+template<typename T>
+auto constexpr IsSame_v = IsSame<T>::value;
+
+
 
 
 
